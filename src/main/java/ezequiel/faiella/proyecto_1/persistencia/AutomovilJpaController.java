57d39433961808e34dc940b79ac77fb83775655d
Fayu <r826720@hotmail.com>
2023-06-11 10:43:27 -0300

    modificacion leve

--- src/main/java/ezequiel/faiella/proyecto_1/persistencia/AutomovilJpaController.java
+++ src/main/java/ezequiel/faiella/proyecto_1/persistencia/AutomovilJpaController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ezequiel.faiella.proyecto_1.persistencia;
 
 import ezequiel.faiella.proyecto_1.logica.Automovil;
